6348334a145cada3fcbc3f2b95f046027524a6aa
shreyas <shreyas@10.0.1.25>
2019-06-30 20:13:17 -0700

    Made console-based

--- Chess/src/Tester.java
+++ Chess/src/Tester.java
@@ -2,6 +2,15 @@
 public class Tester {
 	public static void main(String[] args)
 	{
-		//Grid board = new Grid("data/initial.txt");
+		Grid board = new Grid("data/initial.txt");
+		System.out.println(board);
+		board.move("e4");
+		System.out.println(board);
+		board.move("e5");
+		System.out.println(board);
+		board.move("d4");
+		System.out.println(board);
+		board.move("d5");
+		System.out.println(board);
 	}
 }
